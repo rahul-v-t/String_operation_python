de1698e9651f881ec5e2d2fa759f35d4cc0c8fcd
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-11-11 18:06:04 +0530

    New One

---
+++ practice.py
@@ -0,0 +1,23 @@
+string = "Welcome to Talrop"
+
+
+# 1, To show type of string
+print(type(string))
+# 2, Print the first character from the string
+print(string[0])
+# 3, Split the string in to words
+print(string.split())
+# 4, Convert the String to Uppercase
+print(string.upper())
+# 5, Convert the String to Lowercase
+print(string.lower())
+# 6, Check whether "Talrop" is in the string.
+print("Talrop" in string)
+# 7, How many times "Talrop" repeated in this string 
+print(string.count("Talrop"))
+# 8, Capitalize the first letter 
+print(string.capitalize())
+# 9, Find length of the string
+print(len(string))
+# 10, Replace the spaces with hyphen(-)
+print(string.replace(" ","-"))
---
+++ slice.py
@@ -0,0 +1,4 @@
+string = "Hai Hello How are you"
+print(string[10])
+print(string[3:7])
+print(string[1:7])
